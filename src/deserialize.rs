0dd1e64f1047c9694866683157639a65978a998e
Ben Ruijl <benruyl@gmail.com>
2020-08-06 10:08:01 +0200

    Added f128 and cpython support

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -15,6 +15,12 @@ use pyo3::types::{PyAny, PyFloat, PyList, PySequence, PyTuple};
 #[cfg(feature = "pyo3_support")]
 use pyo3::{FromPyObject, PyObject, PyResult, Python, ToPyObject};
 
+#[cfg(feature = "cpython_support")]
+use cpython::{
+    exc, FromPyObject, PyDrop, PyErr, PyFloat, PyList, PyObject, PyResult, PySequence, PyTuple,
+    Python, PythonObject, ToPyObject,
+};
+
 #[cfg(feature = "serde_support")]
 struct LorentzVectorVisitor<T: Field> {
     _marker: PhantomData<fn() -> LorentzVector<T>>,
@@ -155,6 +161,129 @@ impl<'s> FromPyObject<'s> for LorentzVector<f64> {
             Ok(LorentzVector::from_slice(&v))
         } else {
             pyo3::exceptions::TypeError::into("Invalid list length for LorentzVector conversion")
+        }
+    }
+}
+
+#[cfg(feature = "cpython_support")]
+impl ToPyObject for LorentzVector<f64> {
+    type ObjectType = PyList;
+
+    fn to_py_object(&self, py: Python) -> PyList {
+        PyList::new(
+            py,
+            &[
+                PyFloat::new(py, self.t).into_object(),
+                PyFloat::new(py, self.x).into_object(),
+                PyFloat::new(py, self.y).into_object(),
+                PyFloat::new(py, self.z).into_object(),
+            ],
+        )
+    }
+}
+
+#[cfg(feature = "cpython_support")]
+impl ToPyObject for LorentzVector<Complex<f64>> {
+    type ObjectType = PyList;
+
+    fn to_py_object(&self, py: Python) -> PyList {
+        PyList::new(
+            py,
+            &[
+                PyTuple::new(
+                    py,
+                    &[
+                        PyFloat::new(py, self.t.re).into_object(),
+                        PyFloat::new(py, self.t.im).into_object(),
+                    ],
+                )
+                .into_object(),
+                PyTuple::new(
+                    py,
+                    &[
+                        PyFloat::new(py, self.x.re).into_object(),
+                        PyFloat::new(py, self.x.im).into_object(),
+                    ],
+                )
+                .into_object(),
+                PyTuple::new(
+                    py,
+                    &[
+                        PyFloat::new(py, self.y.re).into_object(),
+                        PyFloat::new(py, self.y.im).into_object(),
+                    ],
+                )
+                .into_object(),
+                PyTuple::new(
+                    py,
+                    &[
+                        PyFloat::new(py, self.z.re).into_object(),
+                        PyFloat::new(py, self.z.im).into_object(),
+                    ],
+                )
+                .into_object(),
+            ],
+        )
+    }
+}
+
+#[cfg(feature = "cpython_support")]
+impl<'s> FromPyObject<'s> for LorentzVector<Complex<f64>> {
+    fn extract(py: Python, obj: &'s PyObject) -> PyResult<Self> {
+        let seq = obj.cast_as::<PySequence>(py)?;
+        let mut v = Vec::new();
+        for item in seq.iter(py)? {
+            let item = item?;
+            let seq = item.cast_as::<PySequence>(py)?;
+            v.push((
+                f64::extract(py, &seq.get_item(py, 0)?)?,
+                f64::extract(py, &seq.get_item(py, 1)?)?,
+            ));
+            item.release_ref(py);
+        }
+
+        if v.len() == 3 {
+            Ok(LorentzVector::from_args(
+                Complex::new(0., 0.),
+                Complex::new(v[0].0, v[0].1),
+                Complex::new(v[1].0, v[1].1),
+                Complex::new(v[2].0, v[2].1),
+            ))
+        } else if v.len() == 4 {
+            Ok(LorentzVector::from_args(
+                Complex::new(v[0].0, v[0].1),
+                Complex::new(v[1].0, v[1].1),
+                Complex::new(v[2].0, v[2].1),
+                Complex::new(v[3].0, v[3].1),
+            ))
+        } else {
+            Err(PyErr::new::<exc::TypeError, _>(
+                py,
+                "Invalid list length for LorentzVector conversion",
+            ))
+        }
+    }
+}
+
+impl<'s> FromPyObject<'s> for LorentzVector<f64> {
+    fn extract(py: Python, obj: &'s PyObject) -> PyResult<Self> {
+        let seq = obj.cast_as::<PySequence>(py)?;
+        let mut v = Vec::new();
+        for item in seq.iter(py)? {
+            let item = item?;
+            v.push(f64::extract(py, &item)?);
+            item.release_ref(py);
+        }
+
+        if v.len() == 3 {
+            Ok(LorentzVector::from_args(0., v[0], v[1], v[2]))
+        } else if v.len() == 4 {
+            Ok(LorentzVector::from_slice(&v))
+        } else {
+            Err(PyErr::new::<exc::TypeError, _>(
+                py,
+                "Invalid list length for LorentzVector conversion",
+            ))
         }
     }
 }
